dcd791eb146c530352b018d29f9081377df870bc
VEERENDRA VAMSHI <136776337+MKVEERENDRA@users.noreply.github.com>
2025-03-19 18:39:14 +0530

    Update app.js

--- Web Panel/app.js
+++ Web Panel/app.js
@@ -1,16 +1,10 @@
-///Edit Under This only
-
-
-    var firebaseConfig = {
-    apiKey: "API KEY",
-    authDomain: "AUTH DOMAIN",
-    databaseURL: "DATABSE URL",
-    projectId: "PROJECT ID",
-    storageBucket: "BUCKET URL",
-    messagingSenderId: "SENDER ID",
-    appId: "APP ID"
-  }; 
- 
-////Do Not Edit Below This
-
-firebase.initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyB7nEVYIXJ1y27Nm-Sd_7jPGTMfzdURNuI",
+  authDomain: "vxavi-bafa9.firebaseapp.com",
+  databaseURL: "https://vxavi-bafa9-default-rtdb.firebaseio.com",
+  projectId: "vxavi-bafa9",
+  storageBucket: "vxavi-bafa9.firebasestorage.app",
+  messagingSenderId: "191967368171",
+  appId: "1:191967368171:web:f1c4d71fc8dd486e8c3fae",
+  measurementId: "G-T6RXQM8V9D"
+};
